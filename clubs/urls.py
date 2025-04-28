af24ffbf0e0539075f17a327fb7dd03d6868d008
RaviPatw <155745992+RaviPatw@users.noreply.github.com>
2025-04-28 14:10:26 +0000

    urls for club

--- clubs/urls.py
+++ clubs/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.club_list, name='club_list'),
-    path('club', views.club_details, name='club_details')
-]
+    path('<int:club_id>', views.club_details, name='club_details')
+    ]
