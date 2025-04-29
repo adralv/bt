b35c6a979cde6f52eee27994c30f77f9af85ac58
RaviPatw <155745992+RaviPatw@users.noreply.github.com>
2025-04-29 19:04:42 -0400

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 # Create your views here.
 @login_required
 def index(request):
-    return render(request, 'index.html')
+    user = request.user
+    context={
+        'user':user
+    }
+    return render(request, 'index.html',context)
 @login_required
 def contact(request):
     return render(request, 'contact.html')
