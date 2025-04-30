89d41b8700e78204a4d5ccad0793bcf935bb5a8e
RaviPatw <155745992+RaviPatw@users.noreply.github.com>
2025-04-30 19:40:42 -0400

    Update models.py

--- clubs/models.py
+++ clubs/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+# from events.models import Event
+
 # Create your models here.
 class Club(models.Model):
     club_name= models.CharField(max_length=100)
@@ -9,4 +11,8 @@ class Club(models.Model):
     image = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     email= models.EmailField((""), max_length=250)
     teacher=models.CharField(max_length=100, default='null')
+    meeting_day=models.TextField(blank=True)
+    # announcments= models.ManyToManyField
+    
+    
     # actvities=models.ArrayField(base_field=)
