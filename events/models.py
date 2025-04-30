43d09539158f8b260aab8d9fa64123a8c45524e4
RaviPatw <155745992+RaviPatw@users.noreply.github.com>
2025-04-30 19:41:52 -0400

    Update models.py

--- events/models.py
+++ events/models.py
@@ -2,11 +2,17 @@ from django.db import models
 from clubs.models import Club
 
 # Create your models here.
-class Event(models.model):
+class Announcements(models.Model):
     # club_id = Put Club ID from club model
+    club = models.ForeignKey(Club, on_delete=models.CASCADE)
     title = models.CharField()
     description = models.CharField()
     date = models.DateTimeField()
     location = models.IntegerField()
+    
+    
+    def __str__(self):
+        return self.date.strftime('%A, %B %d, %I:%M %p')
+
     # publisher = Put User ID
     
