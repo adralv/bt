3279be94c64a97dca1f9952e8af0e505132155e1
RaviPatw <155745992+RaviPatw@users.noreply.github.com>
2025-04-30 22:11:23 -0400

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from clubs.models import Club
+from users.models import UserProfile
 # Create your views here.
 @login_required
 def index(request):
     user = request.user
-    context={
-        'user':user
+    clubs = Club.objects.all()
+    
+    try:
+        user_profile = UserProfile.objects.get(user=user)
+    except UserProfile.DoesNotExist:
+        user_profile = None  
+    
+    context = {
+        'user': user,
+        'clubs': clubs,
+        'user_profile': user_profile
     }
-    return render(request, 'index.html',context)
+    return render(request, 'index.html', context)
+
 @login_required
 def contact(request):
     return render(request, 'contact.html')
