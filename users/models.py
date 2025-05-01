3a5b2f4107acea9984973212c65c6bb2423a1f3e
RaviPatw <155745992+RaviPatw@users.noreply.github.com>
2025-04-30 22:11:47 -0400

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,12 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
-class User(models.Model):
-    username= models.CharField(max_length=100)
-    name = models.CharField(max_length=250)
-    email = models.EmailField((""), max_length=250)
-    password= models.CharField(max_length=250)
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     graduation_year = models.IntegerField()
     profile_picture = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    
-    
