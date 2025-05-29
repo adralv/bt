d93985261c651c987aa17e72e9b4d075a5c420c9
RaviPatw <155745992+RaviPatw@users.noreply.github.com>
2025-05-28 21:22:43 -0400

    Update urls.py

--- clubs/urls.py
+++ clubs/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.club_list, name='club_list'),
-    path('<int:club_id>', views.club_details, name='club_details')
+    path('join/<int:club_id>/',views.join_club, name="join_club"),
+    path('<int:club_id>', views.club_details, name='club_details'),
     ]
